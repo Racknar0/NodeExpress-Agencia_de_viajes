0fe674da1c9d33c6b56d9ce94661a08e6999b4cb
Racknar0 <racknarow1@gmail.com>
2022-03-06 22:54:26 -0500

    add Footer

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ const port = process.env.PORT || 4000;
 // Habilitar PUG
 app.set('view engine', 'pug');
 
+// Obtener año actual
+app.use( (req, res, next) => {
+    const year = new Date()
+
+    res.locals.ActualYear = year.getFullYear()
+    return next();
+});
+
 // Definir la carpeta publica
 app.use(express.static('public'));
 
