2681c927ca7d87ffbe069c628eed9ffd0fa06edb
Racknar0 <racknarow1@gmail.com>
2022-03-07 13:33:09 -0500

    Show testimoniales

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -1,4 +1,5 @@
 import { Viaje } from '../models/Viaje.js'
+import { Testimonial } from '../models/Testimoniales.js'
 
 
 const paginaInicio = (req, res) => { // req - lo que enviamos : res - lo que express nos responde
@@ -26,10 +27,22 @@ const paginaViajes = async (req, res) => {
     })
 }
 
-const paginaTestimoniales = (req, res) => { 
-    res.render('testimoniales', {
-        pagina: 'Testimoniales'
-    })
+const paginaTestimoniales = async (req, res) => { 
+    
+    try {
+        const testimoniales = await Testimonial.findAll();
+
+        res.render('testimoniales', {
+            pagina: 'Testimoniales',
+            testimoniales
+        })
+        
+    } catch (error) {
+        
+        console.log(error);
+
+    }
+
 }
 
 //! Muestra un viaje por su Slug
