e519b22ec0410496af0a067c8eedeb1853962530
Racknar0 <racknarow1@gmail.com>
2022-03-07 14:56:05 -0500

    Env Variables

--- config/db.js
+++ config/db.js
@@ -1,8 +1,11 @@
 import Sequelize from "sequelize";
+import dotenv from 'dotenv';
+dotenv.config({path:"variables.env"});
 
-const db = new Sequelize('agenciaviajes', 'root', 'camilo2676', {
-    hots: '127.0.0.1',
-    port: '3306',
+
+const db = new Sequelize(process.env.DB_NOMBRE, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: 'mysql',
     define: {
         timestamps: false
