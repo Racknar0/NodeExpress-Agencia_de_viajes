ece86fc10907ee40937043f21d7ff08bfcd6d24c
Racknar0 <racknarow1@gmail.com>
2022-03-07 14:20:16 -0500

    Add Home

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -2,11 +2,24 @@ import { Viaje } from '../models/Viaje.js'
 import { Testimonial } from '../models/Testimoniales.js'
 
 
-const paginaInicio = (req, res) => { // req - lo que enviamos : res - lo que express nos responde
-    res.render('inicio', {
-        pagina: 'Inicio',
-        clase: 'home'
-    })
+const paginaInicio = async (req, res) => { // req - lo que enviamos : res - lo que express nos responde
+
+    // Consultar tres viajes del modelo Viaje
+
+    try {
+        const viajes = await Viaje.findAll({ limit: 3 })
+
+        res.render('inicio', {
+            pagina: 'Inicio',
+            clase: 'home',
+            viajes
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+
+    
 }
 
 const paginaNosotros = (req, res) => { 
